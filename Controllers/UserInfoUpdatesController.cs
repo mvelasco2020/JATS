e3ba3b3c5a26dd957319e97f84b69b191d82f581
mike velasco <mvelasco2001@hotmail.com>
2022-07-30 20:54:52 -0500

    finished Profile manage page

--- Controllers/UserInfoUpdatesController.cs
+++ Controllers/UserInfoUpdatesController.cs
@@ -43,5 +43,41 @@ namespace JATS.Controllers
 
             return LocalRedirect("/Identity/Account/Manage");
         }
+
+        public async Task<IActionResult> UpdateProfileInfo(JTUser formInput)
+        {
+
+            try
+            {
+                TempData["SuccessMessage"] = "Successfully saved changes to:";
+                var userId = await _userManager.GetUserAsync(User);
+                JTUser currentUser = _context.Users.FirstOrDefault(u => u.Id == userId.Id);
+                if (currentUser.FirstName != formInput.FirstName)
+                {
+                    currentUser.FirstName = formInput.FirstName;
+                    TempData["SuccessMessage"] += " First Name";
+                }
+                if (currentUser.LastName != formInput.LastName)
+                {
+                    currentUser.LastName = formInput.LastName;
+                    TempData["SuccessMessage"] += " Last Name";
+                }
+                if (currentUser.PhoneNumber != formInput.PhoneNumber)
+                {
+                    currentUser.PhoneNumber = formInput.PhoneNumber;
+                    TempData["SuccessMessage"] += " Phone Number";
+                }
+                await _context.SaveChangesAsync();
+                return LocalRedirect("/Identity/Account/Manage");
+            }
+            catch (Exception)
+            {
+                ViewData["ErrorMessage"] = "Something went wrong while saving the changes, Please check verify if you First Name, Last Name and Phonenumber is valid";
+                return LocalRedirect("/Identity/Account/Manage");
+            }
+
+
+        }
+
     }
 }
